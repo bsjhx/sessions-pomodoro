92b9388c844c4e7c96a66cd6d5ac8858558574d5
bsjhx <rakoczymichal@hotmail.com>
2023-12-02 22:50:40 +0100

    Added some basic implementation of states

--- src-tauri/src/sessions.rs
+++ src-tauri/src/sessions.rs
@@ -3,15 +3,100 @@ struct ApplicationContext {
     state: Box<dyn State>,
 }
 
-struct SessionsCycle {}
+pub struct SessionsCycle {}
 
-pub struct StoppedState;
-pub struct PausedState;
-pub struct PlayingState;
+#[derive(Debug)]
+pub struct NothingState;
+
+#[derive(Debug)]
+pub struct WorkingTimeState;
 
 pub trait State {
-    fn start_cycle(self: Box<Self>, player: &mut SessionsCycle) -> Box<dyn State>;
-    fn finish_cycle(self: Box<Self>, player: &mut SessionsCycle) -> Box<dyn State>;
+    fn get_state_name(&self) -> String;
+    fn start_cycle(self: Box<Self>, cycle: &mut SessionsCycle) -> Box<dyn State>;
+    fn finish_cycle(self: Box<Self>, cycle: &mut SessionsCycle) -> Box<dyn State>;
 
-    fn end(self: Box<Self>, player: &mut SessionsCycle) -> Box<dyn State>;
+    fn end(self: Box<Self>, cycle: &mut SessionsCycle) -> Box<dyn State>;
+}
+
+/// Starting point of application, initial value for cycle.
+impl State for NothingState {
+    fn get_state_name(&self) -> String {
+        "NothingState".to_string()
+    }
+
+    /// Only this method matters in NothingState
+    fn start_cycle(self: Box<Self>, cycle: &mut SessionsCycle) -> Box<dyn State> {
+        Box::new(WorkingTimeState)
+    }
+
+    /// We can't finish not-started state
+    fn finish_cycle(self: Box<Self>, cycle: &mut SessionsCycle) -> Box<dyn State> {
+        self
+    }
+
+    /// We can't end not-started state
+    fn end(self: Box<Self>, cycle: &mut SessionsCycle) -> Box<dyn State> {
+        self
+    }
+}
+
+/// First state after cycle is started. It means, that user is working.
+impl State for WorkingTimeState {
+    fn get_state_name(&self) -> String {
+        "WorkingTimeState".to_string()
+    }
+    /// Does nothing, can' start already started cycle
+    fn start_cycle(self: Box<Self>, cycle: &mut SessionsCycle) -> Box<dyn State> {
+        self
+    }
+
+    /// Finishes cycle, returns to nothing state.
+    fn finish_cycle(self: Box<Self>, cycle: &mut SessionsCycle) -> Box<dyn State> {
+        Box::new(NothingState)
+    }
+
+    /// So far so good. It should head to break state
+    fn end(self: Box<Self>, cycle: &mut SessionsCycle) -> Box<dyn State> {
+        Box::new(NothingState)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::sessions::{NothingState, SessionsCycle, State, WorkingTimeState};
+
+    #[test]
+    fn nothing_state_should_be_able_to_change_state() {
+        // Arrange
+        let state = Box::new(NothingState);
+        let mut cycle = SessionsCycle {};
+
+        // Act & Assert
+        let state = state.finish_cycle(&mut cycle);
+        assert_eq!(state.get_state_name(), "NothingState");
+
+        let state = state.end(&mut cycle);
+        assert_eq!(state.get_state_name(), "NothingState");
+
+        let state = state.start_cycle(&mut cycle);
+        assert_eq!(state.get_state_name(), "WorkingTimeState");
+    }
+
+    #[test]
+    fn working_time_state_should_be_able_to_change_state() {
+        // Arrange
+        let state = Box::new(WorkingTimeState);
+        let mut cycle = SessionsCycle {};
+
+        // Act & Assert
+        let state = state.finish_cycle(&mut cycle);
+        assert_eq!(state.get_state_name(), "NothingState");
+
+        let state = state.end(&mut cycle);
+        assert_eq!(state.get_state_name(), "NothingState");
+
+        let state = state.start_cycle(&mut cycle);
+        assert_eq!(state.get_state_name(), "WorkingTimeState");
+    }
 }
