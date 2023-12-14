868b586dc6596a05c63387142ed15c7659a8082d
bsjhx <rakoczymichal@hotmail.com>
2023-12-14 21:21:30 +0100

    Start and stop works on UI.

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,19 +1,42 @@
 <script>
     import {invoke} from '@tauri-apps/api/tauri'
 
-    let currentState = ["NothingState", "Biedronka"];
+    export let currentState = "NothingState";
+    export let interval = 0;
+    export let timeDisplay = "00:00";
 
-    async function start_cycle() {
-        let new_state = await invoke('start_cycle');
-        currentState = [...currentState, new_state];
+    async function startCycle() {
+        currentState = await invoke('start_cycle');
+        interval = setInterval(increment, 50);
     }
+
+    async function stopCycle() {
+        // currentState = await invoke('start_cycle');
+        clearInterval(interval);
+    }
+
+    function displayTimer(i) {
+        let seconds = i % 60;
+        let minutes = Math.floor(i/60) % 3600;
+        return  "".concat(renderTimeNumber(minutes), ':', renderTimeNumber(seconds));
+    }
+
+    function renderTimeNumber(n) {
+        return n < 10 ? "".concat('0', n.toString()) : n.toString();
+    }
+
+    export let i = 0
+
+    function increment() {
+        i++;
+        timeDisplay = displayTimer(i);
+    }
+
 </script>
 
 <div>
-    <button on:click="{start_cycle}">Greet</button>
-    <ol>
-        {#each currentState as state}
-            <li>{state}</li>
-        {/each}
-    </ol>
+    <h3>Pomodoro</h3>
+    <button on:click="{startCycle}">Start cycle</button>
+    <button on:click="{stopCycle}">Stop cycle</button>
+    {timeDisplay}
 </div>
