0c344b99223af821c9ea7056bb2fc1ebe4679afe
bsjhx <rakoczymichal@hotmail.com>
2024-03-04 20:53:54 +0100

    Added basic implementation

--- src-tauri/src/db/history_db.rs
+++ src-tauri/src/db/history_db.rs
@@ -1,6 +1,7 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Timelike, Utc};
 use r2d2::PooledConnection;
 use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite::params;
 
 pub struct StateHistoryItem {
     id: String,
@@ -23,6 +24,20 @@ impl HistoryStatesDbSqliteImpl {
 
 impl HistoryStatesDb for HistoryStatesDbSqliteImpl {
     fn get_states_history_by_date(&self, date: DateTime<Utc>) -> Vec<StateHistoryItem> {
-        todo!()
+        let from_midnight = date.date_naive().and_hms_opt(0, 0, 0).unwrap().and_utc();
+        let to_midnight = date.date_naive().and_hms_opt(23, 59, 59).unwrap().and_utc();
+        let query =
+            "select state_id, started_time from states where started_time between ?1 and ?2";
+        let mut stmt = self.connection.prepare(query).unwrap();
+        let result = stmt
+            .query_map(params![from_midnight, to_midnight], |row| {
+                Ok(StateHistoryItem {
+                    id: row.get(0).unwrap_or_default(),
+                    time: row.get(1).unwrap_or_default(),
+                })
+            })
+            .unwrap();
+
+        result.into_iter().map(|s| s.unwrap()).collect()
     }
 }
